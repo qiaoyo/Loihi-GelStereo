ea7c114208a24bf9b2cdce91c6360d725bd88534
qiaoyo <13610678931@163.com>
2022-02-07 16:14:24 +0800

    add commentary

--- gelstereo.py
+++ gelstereo.py
@@ -6,75 +6,56 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import slayerSNN as snn
 
-from slayerSNN import learningStats as learningStats
-from slayerSNN import optimizer as optimizer
+from slayerSNN.spikeFileIO import event
 
-class Event():
+def read2Dspikes(path):
 
-    def __init__(self, xEvent, yEvent, pEvent, tEvent):
-        if yEvent is None:
-            self.dim = 1
-        else:
-            self.dim = 2
+    '''
 
-        self.x = xEvent if type(xEvent) is np.ndarray else np.asarray(xEvent)  # x spatial dimension
-        self.y = yEvent if type(yEvent) is np.ndarray else np.asarray(yEvent)  # y spatial dimension
-        self.p = pEvent if type(pEvent) is np.ndarray else np.asarray(pEvent)  # spike polarity
-        self.t = tEvent if type(tEvent) is np.ndarray else np.asarray(tEvent)  # time stamp in ms
+    :param path: bs2 file path
+    :return: event(Class format) from slayerSNN.spikeFileIO
+    '''
 
-        if not issubclass(self.x.dtype.type, np.integer): self.x = self.x.astype('int')
-        if not issubclass(self.p.dtype.type, np.integer): self.p = self.p.astype('int')
+    file_size = os.path.getsize(path)
+    if file_size == 0:
+        print('empty event data file.')
+        return None, 0
+    if file_size % 8 != 0:
+        print('corrupted event data file.', file_size)
+        return None, 0
 
-        if self.dim == 2:
-            if not issubclass(self.y.dtype.type, np.integer): self.y = self.y.astype('int')
+    with open(path, 'rb') as f:
+        data = f.read(file_size)
 
-        self.p -= self.p.min()
+    cnt = file_size // 8
 
-class snnio_update_loadbin(snn.io):
-    def read2Dspikes(path):
+    idx = 0
+    start_ts = data[0] & 0x7FFFFFFF
 
-        file_size = os.path.getsize(path)
-        if file_size == 0:
-            print('empty event data file.')
-            return None, 0
-        if file_size % 8 != 0:
-            print('corrupted event data file.', file_size)
-            return None, 0
+    xEvent, yEvent, pEvent, tEvent = [], [], [], []
 
-        with open(path, 'rb') as f:
-            data = f.read(file_size)
+    for i in range(cnt):
+        datum = (data[i * 8] << 56) | (data[i * 8 + 1] << 48) | (data[i * 8 + 2] << 40) | (
+                    data[i * 8 + 3] << 32) | (data[i * 8 + 4] << 24) | (data[i * 8 + 5] << 16) | (
+                            data[i * 8 + 6] << 8) | (data[i * 8 + 7])
 
-        cnt = file_size // 8
+        x = np.uint16(datum >> 48)
+        y = np.uint16((datum >> 32) & 0xffff)
+        p = (datum >> 31) & 0x1
+        t = np.uint32(datum & 0x7FFFFFFF)
 
-        idx = 0
-        start_ts = data[0] & 0x7FFFFFFF
+        xEvent.append(x)
+        yEvent.append(y)
+        pEvent.append(p)
+        tEvent.append(t)
+        idx += 1
 
-        xEvent, yEvent, pEvent, tEvent = [], [], [], []
+    duration = data[-1] & 0xFFFFFFFF
+    events = event(xEvent, yEvent, pEvent, [time  for time in tEvent])
 
-        for i in range(cnt):
-            datum = (data[i * 8] << 56) | (data[i * 8 + 1] << 48) | (data[i * 8 + 2] << 40) | (
-                        data[i * 8 + 3] << 32) | (data[i * 8 + 4] << 24) | (data[i * 8 + 5] << 16) | (
-                                data[i * 8 + 6] << 8) | (data[i * 8 + 7])
+    return events
 
-            x = np.uint16(datum >> 48)
-            y = np.uint16((datum >> 32) & 0xffff)
-            p = (datum >> 31) & 0x1
-            t = np.uint32(datum & 0x7FFFFFFF)
 
-            xEvent.append(x)
-            yEvent.append(y)
-            pEvent.append(p)
-            tEvent.append(t)
-            idx += 1
-
-        duration = data[-1] & 0xFFFFFFFF
-        events = Event(xEvent, yEvent, pEvent, [time / 1000 for time in tEvent])
-
-        print(type(events))
-
-        return events
-
-netParams=snn.params('network.yaml')
 
 ## load dataset
 class gelstereoDataset(Dataset):
@@ -88,12 +69,12 @@ class gelstereoDataset(Dataset):
         inputIndex=self.samples[index,0]
         classLabel=self.samples[index,1]
 
-        inputSpikes=snnio_update_loadbin.read2Dspikes(self.path+str(inputIndex.item())+'.bs2').toSpikeTensor(torch.zeros((2,25,25,self.nTimeBins)))
+        inputSpikes=read2Dspikes(os.path.join(self.path,str(inputIndex.item())+'.bs2')).toSpikeTensor(torch.zeros((2,800,800,self.nTimeBins)))
 
         desiredClass=torch.zeros((2,1,1,1))
         desiredClass[classLabel,...]=1
 
-        return inputSpikes.reshape((-1,1,1,inputSpikes.shape[-1])),desiredClass,classLabel
+        return inputSpikes,desiredClass,classLabel
 
     def __len__(self):
         return self.samples.shape[0]
@@ -105,90 +86,115 @@ class Network(torch.nn.Module):
         super(Network,self).__init__()
         slayer=snn.layer(netParams['neuron'],netParams['simulation'])
         self.slayer=slayer
-
-        self.fc1 = slayer.dense((25,25,2),512)
+        self.conv1=slayer.conv(2,16,5,padding=2,weightScale=10)
+        self.conv2=slayer.conv(16,32,3,padding=1,weightScale=20)
+        self.pool1=slayer.pool(10)
+        self.pool2=slayer.pool(2)
+        self.pool3=slayer.pool(2)
+        self.fc1 = slayer.dense((20,20,32),512)
         self.fc2 = slayer.dense(512,2)
+        self.drop=slayer.dropout(0.1)
 
     def forward(self,spikeInput):
-        spikeLayer1=self.slayer.spike(self.slayer.psp(self.fc1(spikeInput)))
-        spikeLayer2=self.slayer.spike(self.slayer.psp(self.fc2(spikeLayer1)))
+        spike=self.slayer.spike(self.pool1(spikeInput)) #80,80,2
+        spike=self.drop(spike)
+        spike=self.slayer.spike(self.conv1(spike)) #80,80,16
+        spike=self.slayer.spike(self.pool2(spike)) #40,40,16
+        spike=self.drop(spike)
+        spike=self.slayer.spike(self.conv2(spike)) #40,40,32
+        spike=self.slayer.spike(self.pool3(spike)) #20,20,32
+        spike=self.drop(spike)
 
-        return spikeLayer2
+        spike=self.slayer.spike(self.slayer.psp(self.fc1(spike)))
+        spike=self.slayer.spike(self.slayer.psp(self.fc2(spike)))
 
+        return spike
 
-device = torch.device('cuda')
-net=Network(netParams).to(device)
+if __name__ == "__main__"  :
 
-error=snn.loss(netParams).to(device)
+    netParams=snn.params('network.yaml')
 
-optimizer=torch.optim.Adam(net.parameters(),lr=0.01,amsgrad=True)
+    print(torch.cuda.is_available())
 
-trainingSet = gelstereoDataset(datasetPath=netParams['training']['path']['in'],
-                            sampleFile=netParams['training']['path']['train'],
-                            samplingTime=netParams['simulation']['Ts'],
-                            sampleLength=netParams['simulation']['tSample'])
-trainLoader = DataLoader(dataset=trainingSet, batch_size=8, shuffle=False, num_workers=4)
+    device = torch.device('cuda')
+    net=Network(netParams).to(device)
+    net=torch.nn.DataParallel(net,device_ids=[0,1,2,3])
 
-testingSet = gelstereoDataset(datasetPath=netParams['training']['path']['in'],
-                           sampleFile=netParams['training']['path']['test'],
-                           samplingTime=netParams['simulation']['Ts'],
-                           sampleLength=netParams['simulation']['tSample'])
-testLoader = DataLoader(dataset=testingSet, batch_size=8, shuffle=False, num_workers=4)
+    error=snn.loss(netParams).to(device)
 
-stats=learningStats()
+    optimizer = snn.utils.optim.Nadam(net.parameters(), lr = 0.01, amsgrad = True)
 
-for epoch in range(100):
-    tst=datetime.now()
+    trainingSet = gelstereoDataset(datasetPath=netParams['training']['path']['in'],
+                                sampleFile=netParams['training']['path']['train'],
+                                samplingTime=netParams['simulation']['Ts'],
+                                sampleLength=netParams['simulation']['tSample'])
+    trainLoader = DataLoader(dataset=trainingSet, batch_size=8, shuffle=False, num_workers=1)
 
-    for i ,(input,output,label) in enumerate(trainLoader,0):
-        input = input.to(device)
-        target = target.to(device)
+    testingSet = gelstereoDataset(datasetPath=netParams['training']['path']['in'],
+                               sampleFile=netParams['training']['path']['test'],
+                               samplingTime=netParams['simulation']['Ts'],
+                               sampleLength=netParams['simulation']['tSample'])
+    testLoader = DataLoader(dataset=testingSet, batch_size=8, shuffle=False, num_workers=1)
 
-        output=net.forward(input)
+    stats = snn.utils.stats()
 
-        stats.training.correctSamples +=torch.sum(snn.predict.getClass(output) == label ).data.item()
-        stats.training.numSamples +=len(label)
+    for epoch in range(50):
+        tst=datetime.now()
 
-        loss=error.numSpikes(output,target)
+        for i ,(input,target,label) in enumerate(trainLoader,0):
+            net.train()
+            input = input.to(device)
+            target = target.to(device)
 
-        optimizer.zero_grad()
+            output=net.forward(input)
 
-        loss.backward()
+            stats.training.correctSamples +=torch.sum(snn.predict.getClass(output) == label ).data.item()
+            stats.training.numSamples +=len(label)
 
-        optimizer.step()
+            loss=error.numSpikes(output,target)
 
-        stats.training.lossSum+= loss.cpu().data.item()
+            optimizer.zero_grad()
 
+            loss.backward()
 
-    for i ,(input,target,label) in enumerate(testLoader,0):
-        input=input.to(device)
-        target=target.to(device)
+            optimizer.step()
 
-        output=net.forward(input)
+            stats.training.lossSum+= loss.cpu().data.item()
+            stats.print(epoch, i, (datetime.now() - tst).total_seconds())
 
-        stats.testing.correctSamples+=torch.sum(snn.predict.getClass(output) == label ).data.item()
-        stats.testing.numSamples += len(label)
+        for i ,(input,target,label) in enumerate(testLoader,0):
+            net.eval()
+            with torch.no_grad():
+                input=input.to(device)
+                target=target.to(device)
 
-        loss = error.numSpikes(output,target)
-        stats.testing.lossSum+=loss.cpu().data.item()
+            output=net.forward(input)
 
-    if epoch%10 ==0: stats.print(epoch,timeElapsed=(datetime.now()-tst).total_seconds())
-    stats.update()
-    if stats.training.bestLoss is True: torch.save(net.state_dict(),'trained_gelstereo.pt')
+            stats.testing.correctSamples+=torch.sum(snn.predict.getClass(output) == label ).data.item()
+            stats.testing.numSamples += len(label)
 
-plt.semilogy(stats.training.lossLog,label='Training')
-plt.semilogy(stats.testing.lossLog,label='Testing')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend()
-plt.savefig('Loss.png')
-plt.clf()
+            loss = error.numSpikes(output,target)
+            stats.testing.lossSum+=loss.cpu().data.item()
 
-plt.plot(stats.training.accuracyLog,label='Training')
-plt.plot(stats.testing.accuracyLog,label='Testing')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.savefig('Accuracy.png')
-stats.save('Trained/')
+        # if epoch%10 ==0: stats.print(epoch,timeElapsed=(datetime.now()-tst).total_seconds())
+        stats.update()
+        if stats.training.bestLoss is True: torch.save(net.state_dict(),'trained_gelstereo.pt')
+
+    # plot the accuracy and loss,save file
+
+    plt.semilogy(stats.training.lossLog,label='Training')
+    plt.semilogy(stats.testing.lossLog,label='Testing')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig('Loss.png')
+    plt.clf()
+
+    plt.plot(stats.training.accuracyLog,label='Training')
+    plt.plot(stats.testing.accuracyLog,label='Testing')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('Accuracy.png')
+    stats.save('Trained/')
 
